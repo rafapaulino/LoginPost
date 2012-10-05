fdbad81f24117f73cb6c3292b67fcee7981b3e40
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-10-05 11:15:35 -0300

    Initial Commit

---
+++ LoginPostTests/LoginPostTests.h
@@ -0,0 +1,13 @@
+//
+//  LoginPostTests.h
+//  LoginPostTests
+//
+//  Created by Rafael Brigagão Paulino on 05/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LoginPostTests : SenTestCase
+
+@end
---
+++ LoginPostTests/LoginPostTests.m
@@ -0,0 +1,32 @@
+//
+//  LoginPostTests.m
+//  LoginPostTests
+//
+//  Created by Rafael Brigagão Paulino on 05/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "LoginPostTests.h"
+
+@implementation LoginPostTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LoginPostTests");
+}
+
+@end
